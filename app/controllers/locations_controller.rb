61be1f0e4b0b6a5269e8b0f17c0bc340ae4aef39
ichi38 <itirou_miha@yahoo.co.jp>
2015-07-18 18:01:53 +0900

    SQLで取得し、javascriptに渡す(json形式)

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,5 +4,8 @@ class LocationsController < ApplicationController
         @longitude = params[:longitude]   #経度
         
         @station = Station.find_by(station_name: '梅島')
+
+        sql = "Select * From stations Where longitude  > #{@longitude}"
+        @station2 = ActiveRecord::Base.connection.select_all(sql)
   end
 end
